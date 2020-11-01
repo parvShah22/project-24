49d606ba3ece8bfed957a2db87116c648d47944d
parvShah22 <71689287+parvShah22@users.noreply.github.com>
2020-11-01 14:03:11 +0530

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,12 +1,26 @@
-class ball{
-    constructor(x,y,width,height){
+class Ball {
+    constructor(x,y,radius){
         var options = {
             'restitution':0.8,
             'friction':0.3,
-            'density':1.0,
+            'density':1.2
         }
+        this.radius = radius
+        this.body = Bodies.circle(x, y,radius , options);
+        
+        World.add(world, this.body);
+      }
+     display(){
+        var pos =this.body.position;
+      
+        
+        
+        
+        ellipseMode(CENTER);
+        fill("red");
+        ellipse(pos.x,pos.y, this.radius, this.radius);
+        
+          };
     }
-
-}
 
 
